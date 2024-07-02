87b9fc3dcc1c93ae8dd1d01640b751a952165c66
Yardan Safir <117962312+Yardan9Safir@users.noreply.github.com>
2024-07-02 19:31:09 +0700

    "test"

--- function.php
+++ function.php
@@ -1,64 +1,31 @@
 <?php
-
-
 $koneksi = mysqli_connect("localhost", "root", "", "dbrestoran");
-
-
 // Funtion Register
-
 function register_akun()
-
 {
-
     global $koneksi;
-
-
-
     $username = htmlspecialchars($_POST["username"]);
-
     $password = md5(htmlspecialchars($_POST["password"]));
-
     $konfirmasi_password = md5(htmlspecialchars($_POST["konfirmasi-password"]));
-
-
-
     $cek_username = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM `user` WHERE username = '$username'"));
-
-
-
     if ($cek_username != null) {
-
         echo "<script>
-
             alert('Username sudah ada!');
 
         </script>";
-
         return -1;
     } else if ($password != $konfirmasi_password) {
-
         echo "<script>
-
             alert('Password Tidak Sesuai!');
-
         </script>";
-
         return -1;
     }
-
-
-
     mysqli_query($koneksi, "INSERT INTO `user`
 
                             VALUES ('', '$username', '$password')
-
     ");
-
     return mysqli_affected_rows($koneksi);
 }
-
-
-
 // Function Login
 
 function login_akun()
